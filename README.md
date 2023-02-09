0774bfe699d88ed20e48656bf0de1c250eaacacf
Alexandro Sanchez Bach <alexandro@phi.nz>
2023-02-09 12:26:09 +0100

    Add project boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # milli-py
-Python bindings for Milli, the embeddable Rust-based search engine behind Meilisearch
+
+Python bindings for Milli, the embeddable Rust-based search engine behind Meilisearch.
+
+Install the package via:
+
+```sh
+pip install milli
+```
---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+extern crate pyo3;
+
+use pyo3::prelude::*;
+
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+#[pymodule]
+fn milli(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    Ok(())
+}
